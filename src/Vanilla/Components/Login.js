2893e12dc25a17d8a92dad1f558b786ea7cb83ca
ClaraWJ <clwj@itu.dk>
2023-04-21 11:03:04 +0200

    made classstyling

--- src/Vanilla/Components/Login.js
+++ src/Vanilla/Components/Login.js
@@ -23,66 +23,59 @@ export default function Main() {
   return (
     <div
       id="container"
-      style={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#f8fafc",
-      }}
-      className=""
+      className="flex flex-col bg-gray-100 h-full"
     >
       <img
         src="/pizzafox.png"
         style={{
-          width: "40%",
-          alignSelf: "flex-end",
-          margin: "1em 1em 4em 1em",
+          //alignSelf: "flex-end",
+          //margin: "1em 1em 4em 1em"
         }}
-        className=""
+        className="w-[40%] self-end mt-[1em] mb-[4em] mr-[1em] ml-[1em]"
       />
       <form
         style={{
-          width: "60%",
-          display: "flex",
-          flexDirection: "column",
-          alignSelf: "center",
+          //width: "60%",
+          //display: "flex",
+          //flexDirection: "column",
+          //alignSelf: "center",
         }}
-        className=""
+        className="w-[60%] flex flex-col self-center"
         onSubmit={handleSubmit}
       >
         <h1
           style={{
-            fontSize: "3rem",
-            lineHeight: "1",
+            //fontSize: "3rem",
+            //lineHeight: "1",
           }}
-          className=""
+          className="text-5xl"
         >
           Sign in
         </h1>
         <p
           style={{
-            fontSize: "1rem",
-            lineHeight: "2rem",
-            color: "#475569",
+            //fontSize: "1rem",
+            //lineHeight: "2rem",
+            //color: "#475569",
           }}
-          className=""
+          className="text-base text-gray-500 mt-2"
         >
           Please sign in to continue
         </p>
         <div
           id="email-input-container"
           style={{
-            display: "flex",
-            flexDirection: "column",
-            marginTop: "1em",
-            borderRadius: "0.75rem",
-            borderWidth: "1px",
-            borderColor: "black",
-            backgroundColor: "white",
+            //display: "flex",
+            //flexDirection: "column",
+            //marginTop: "1em",
+            //borderRadius: "0.75rem",
+            //borderWidth: "1px",
+            //borderColor: "black",
+            //backgroundColor: "white",
             filter:
               "drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))",
           }}
-          className=""
+          className="flex flex-col mt-[1em] borderradius-[0.75rem] border-[1px] border-black rounded-2xl"
         >
           <label
             id="email-label"
